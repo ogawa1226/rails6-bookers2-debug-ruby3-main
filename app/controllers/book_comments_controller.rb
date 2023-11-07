11166c44aea067a273b8a7c712ad05d2f6549b27
EC2 Default User <ec2-user@ip-172-31-5-32.ap-northeast-1.compute.internal>
2023-11-07 10:24:55 +0000

    finish5

--- app/controllers/book_comments_controller.rb
+++ app/controllers/book_comments_controller.rb
@@ -2,19 +2,21 @@ class BookCommentsController < ApplicationController
 
   def create
     book = Book.find(params[:book_id])
-    comment = current_user.book_comments.new(book_comment_params)
-    comment.book_id = book.id
-    comment.save
-    redirect_to request.referer
+    @comment = current_user.book_comments.new(book_comment_params)
+    @comment.book_id = book.id
+    @comment.save
+    render '_create.js.erb'
   end
-  
+
   def destroy
-    comment = BookComment.find(params[:id])
-    comment.destroy
+    @comment = BookComment.find(params[:id])
+    @comment.destroy
+    render '_destroy.js.erb'
+
   end
-  
+
   private
-  
+
   def book_comment_params
     params.require(:book_comment).permit(:comment)
   end
